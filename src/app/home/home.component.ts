f05b41613143c56b7744208e22af0ec1f5f93f54
Emma Pesjak <empe2105@student.miun.se>
2022-12-09 18:34:43 +0100

    added som css and tested getting info from backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Restaurant } from '../restaurant';
+import { BackendService } from '../backend.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+
+
+
+  // allt här ska ju inte vara här, bara testar
+  restaurants: Restaurant[];
+
+  constructor(private backend: BackendService) {
+    this.restaurants = []; 
+  }
+
+   ngOnInit(): void {
+    this.getRestaurants();
+  }
+
+
+  getRestaurants() {
+    this.backend.getRestaurants()
+      .then(restaurants => {
+        this.restaurants = restaurants;
+      })
+      .catch(error => console.error(`An error occurred getting all restaurants: ${error}`));
+  }
 
 }
