0ebfe593bdd7fc75d97d32a9d787c68e166e1b8a
Emma Pesjak <empe2105@student.miun.se>
2022-12-10 13:18:59 +0100

    added restaurant info

--- src/app/restaurant-info/restaurant-info.component.ts
+++ src/app/restaurant-info/restaurant-info.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../backend.service';
+import { Restaurant } from '../restaurant';
 
 @Component({
   selector: 'app-restaurant-info',
   templateUrl: './restaurant-info.component.html',
   styleUrls: ['./restaurant-info.component.css']
 })
+
 export class RestaurantInfoComponent {
+
+    restaurant!: Restaurant | undefined;
+    restaurantName: string = "White Lotus";  //Här ska det inte vara hårdkodat :)
+
+    constructor(private backend: BackendService) {
+      this.restaurant = undefined; 
+      //this.restaurantName = restaurantName
+    }
+  
+    ngOnInit(): void {
+      this.getRestaurant();
+    }
+  
+  
+    getRestaurant() {
+      this.backend.getRestaurant(this.restaurantName)
+        .then(restaurant => {
+          this.restaurant = restaurant;
+        })
+        .catch(error => console.error(`An error occurred getting the restaurant: ${error}`));
+    }
 
 }
